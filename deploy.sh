ecfb8490f00c9adc88ad08fbb346c2996f556b4f
nkhandwe <nkhandwe@gmail.com>
2024-05-13 22:52:39 +0530

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
-git pull
+# git pull
+# yarn run build
+# pm2 delete "6ammart-web-next-js-dev"
+# pm2 start npm --name "6ammart-web-next-js-dev" -- start
+
 yarn run build
-pm2 delete "6ammart-web-next-js-dev"
-pm2 start npm --name "6ammart-web-next-js-dev" -- start
+scp -i misterd.pem  -r .next ubuntu@ec2-16-16-104-192.eu-north-1.compute.amazonaws.com:/home/ubuntu/misterd
+
+
+pm2 start "yarn start" 
+
+pm2 startup
+
+pm2 save
